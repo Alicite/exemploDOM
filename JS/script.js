1b48b4987052bcd246353587a6bcf244491fd95d
Alicite <lunacarter1503@gmail.com>
2026-05-04 22:00:47 -0300

    aula 04/05

--- JS/script.js
+++ JS/script.js
@@ -1,38 +1,13 @@
-const titulo = document.querySelector('h1');
-const paragrafo2 = document.querySelector('#paragrafo')
-const paragrafos = document.querySelectorAll('.paragrafo')
-const botaoClique = document.querySelector('#bt-clique')
-const inputNome = document.querySelector('#input-nome')
+const btEnviar = document.querySelector('#bt-enviar')
+const container = document.querySelector('#container')
 
-function hello(){
-    const nome = inputNome.value
-    alert(`Olá ${nome}!`)
-}
+btEnviar.addEventListener('click', () => {
+    const nome = document.querySelector('#input-nome').value;
+    const jogo = document.querySelector('#input-jogo').value;
+    const nascimento = document.querySelector('#input-nascimento').value;
 
-function helloEnter(evento){
-    if (evento.key == 'Enter'){
-        const nome = inputNome.value
-        alert(`Olá ${nome}!`)
-    } else if (evento.key == 'Alt'){
-        alert('Encontrou a função secreta!!!')
-    }
-}
-
-botaoClique.addEventListener('click', hello)
-inputNome.addEventListener('keyup', helloEnter)
-
-// botaoClique.onclick = () => {
-//     titulo.innerText = "Acabou a aula!"
-    
-//     for (elemento of paragrafos){
-//         elemento.style.color = 'purple'
-//     }
-    
-//     if (document.querySelector('#gatinho') == null) {
-//         const gatinho = document.createElement('img')
-//         gatinho.id = 'gatinho'
-//         gatinho.src = 'https://img.freepik.com/fotos-premium/gatinho-bonito-dos-desenhos-animados-sentado-no-fundo-branco_893012-113909.jpg'
-//         gatinho.style.width = '50px'
-//         document.body.appendChild(gatinho)
-//     }
-// }
+    const novaDiv = document.createElement('div');
+    novaDiv.innerHTML = `<h3>${nome}</h3><p>${jogo}</p><p>${nascimento}</p>`;
+    novaDiv.classList.add('card');
+    container.appendChild(novaDiv);
+})
